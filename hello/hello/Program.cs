cecb699aae4f44984b9dca1458c57c9c15a6596e
h-Anim <scheglovmaxim@gmail.com>
2018-02-19 00:00:48 +0700

    test commit

--- hello/hello/Program.cs
+++ hello/hello/Program.cs
@@ -12,7 +12,7 @@ namespace hello
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Oh, hi, Mark!");
-			Console.WriteLine("No you branch!");
+			Console.WriteLine("I am branch2!");
             Console.ReadKey();
         }
     }
